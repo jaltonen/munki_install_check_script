3a918731c3b4b14e04963a694cdc71827b057e21
John Altonen <jaltonen@gmail.com>
2014-12-12 15:10:38 -0500

    updated documentation

--- Version_check.py
+++ Version_check.py
@@ -7,6 +7,9 @@ from distutils.version import LooseVersion
 #
 #  replace new_version with the version number that you are updating to
 #  plist needs the path to the application in questions info.plist
+#  if the new_version number is greater than the version number in the 
+#  applications info.plist, the script return a 0 to munki and the update 
+#  installs
 #
 ########################################################################
 
