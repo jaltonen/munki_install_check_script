fb6b6bc3bc32e59e1dd3bd2c7523cdb5f5d4cf72
John Altonen <jaltonen@gmail.com>
2014-12-12 15:05:23 -0500

    munki installcheck_script in python
    compares supplied update version number to currently installed version
    number of a specified app.  uses the application info.plist
    CFBundleShortVersionString which seems to be pretty consistent across
    apps

---
+++ Version_check.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+import plistlib
+from distutils.version import LooseVersion
+
+########################################################################
+#
+#  replace new_version with the version number that you are updating to
+#  plist needs the path to the application in questions info.plist
+#
+########################################################################
+
+new_version = "14.4.9"
+plist = plistlib.readPlist('/Applications/Microsoft Office 2011/Microsoft Word.app/Contents/Info.plist')
+
+########################################################################
+
+
+current_version = plist['CFBundleShortVersionString']
+
+
+# if a higher version or equal version is currently installed the following expression returns false
+#   &#60;  replaces < less than in an xml file because < is a special xml character
+install_update = LooseVersion(current_version) &#60; LooseVersion(new_version)
+print install_update
+
+
+if install_update == True:
+#    print 'Im gonna install it'
+    exit(0)
+elif install_update == False:
+#    print 'No go!'
+    exit(1)
