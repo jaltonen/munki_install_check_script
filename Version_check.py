9f73c808a252dd27815b8335c279781ee6110ac3
John Altonen <jaltonen@gmail.com>
2015-03-10 10:21:47 -0400

    account for no current_version
    considers the scenario where no version of the software is currently
    installed.  current_version having the value of null was crashing the
    script

--- Version_check.py
+++ Version_check.py
@@ -1,31 +1,37 @@
-#!/usr/bin/env python
+<key>installcheck_script</key>
+<string>#!/usr/bin/env python	
 import os
 import plistlib
 from distutils.version import LooseVersion
 
-########################################################################
-#
-#  replace new_version with the version number that you are updating to
-#  plist needs the path to the application in questions info.plist
-#  if the new_version number is greater than the version number in the 
-#  applications info.plist, the script return a 0 to munki and the update 
-#  installs
-#
-########################################################################
 
-new_version = "14.4.9"
-plist = plistlib.readPlist('/Applications/Microsoft Office 2011/Microsoft Word.app/Contents/Info.plist')
+#########################################################################################
+#																						#
+#  replace new_version with the version number that you are updating to					#
+#  filename needs the path to the application in questions info.plist					#	
+#  if the new_version number is greater than the version number in the 					#
+#  applications info.plist, the script return a 0 to munki and the update 				#
+#  installs.  If the info.plist file specified does not exist on the target system		#
+#  we are assuming the software is not installed at all and assigning a					#
+#  current_version number of 0.0.1 to approximate that condition, otherwise there		#
+#  were errors thrown with a null value for current_version								#
+#																						#
+#########################################################################################
 
-########################################################################
+new_version = "11.2.1"
+filename = "/Applications/DaVinci Resolve/DaVinci Resolve.app/Contents/Info.plist"
 
+if os.path.exists(filename):
+    plist = plistlib.readPlist(filename)
+    current_version = plist['CFBundleShortVersionString']
+else:
+    current_version = "0.0.1"
 
-current_version = plist['CFBundleShortVersionString']
 
 
 # if a higher version or equal version is currently installed the following expression returns false
-#   &#60;  replaces less than character in an xml file because less than is a special xml character
 install_update = LooseVersion(current_version) &#60; LooseVersion(new_version)
-print install_update
+#print install_update
 
 
 if install_update == True:
@@ -34,3 +40,4 @@ if install_update == True:
 elif install_update == False:
 #    print 'No go!'
     exit(1)
+</string>
